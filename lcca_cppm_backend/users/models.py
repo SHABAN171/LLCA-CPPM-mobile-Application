8f5019ff175ba9d1c96b38587bfd4da2b998d590
SHABAN171 <shabanindekupe@gmail.com>
2026-05-30 08:50:00 +0300

    models.py for users added

--- lcca_cppm_backend/users/models.py
+++ lcca_cppm_backend/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+#model
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('manager', 'Project Manager'),
+        ('analyst', 'Financial Analyst'),
+        ('viewer', 'Viewer'),
+    )
+
+    role = models.CharField(
+        max_length=50,
+        choices=ROLE_CHOICES,
+        default='viewer'
+    )
