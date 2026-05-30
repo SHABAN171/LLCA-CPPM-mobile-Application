27a69a4833ebd1d4b62fc1c94cb78ca836b11120
SHABAN171 <shabanindekupe@gmail.com>
2026-05-30 08:54:48 +0300

    views.py modified

--- lcca_cppm_backend/users/views.py
+++ lcca_cppm_backend/users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import User
+from .serializers import UserSerializer
+
+class RegisterView(generics.CreateAPIView):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
