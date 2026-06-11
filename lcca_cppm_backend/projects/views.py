0b152a2b1c310ae7dfbf7110ef81710bee1d6cb8
SHABAN171 <shabanindekupe@gmail.com>
2026-06-11 17:19:25 +0300

    full project API views added

--- lcca_cppm_backend/projects/views.py
+++ lcca_cppm_backend/projects/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Project
+from .serializers import ProjectSerializer
 
-# Create your views here.
+class ProjectViewSet(viewsets.ModelViewSet):
+
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(manager=self.request.user)
