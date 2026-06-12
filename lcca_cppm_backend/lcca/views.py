703fcfd0ba2f7ff0d9a546ca3c2fb428d4f4b084
SHABAN171 <shabanindekupe@gmail.com>
2026-06-12 08:57:52 +0300

    Add LCCA summary endpoint

--- lcca_cppm_backend/lcca/views.py
+++ lcca_cppm_backend/lcca/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import LifecycleCost
 from .serializers import LifecycleCostSerializer
+
 
 class LifecycleCostViewSet(viewsets.ModelViewSet):
 
     queryset = LifecycleCost.objects.all()
     serializer_class = LifecycleCostSerializer
     permission_classes = [IsAuthenticated]
+
+    @action(detail=True, methods=['get'])
+    def summary(self, request, pk=None):
+
+        record = self.get_object()
+
+        return Response({
+            "project": record.project.title,
+            "total_cost": record.total_lifecycle_cost,
+            "npv": record.npv,
+            "discount_rate": record.discount_rate
+        })
