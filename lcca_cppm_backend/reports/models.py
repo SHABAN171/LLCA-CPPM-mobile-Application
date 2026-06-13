bb2d32b79d688ea173a4d20dc24274f9be4a7a99
SHABAN171 <shabanindekupe@gmail.com>
2026-06-13 08:53:41 +0300

    updated models.py for reports app

--- lcca_cppm_backend/reports/models.py
+++ lcca_cppm_backend/reports/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from projects.models import Project
+from users.models import User
 
-# Create your models here.
+
+class Report(models.Model):
+
+    REPORT_TYPES = (
+        ('lcca', 'LCCA'),
+        ('cppm', 'CPPM'),
+        ('project', 'Project'),
+    )
+
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE
+    )
+
+    report_type = models.CharField(
+        max_length=20,
+        choices=REPORT_TYPES
+    )
+
+    generated_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return f"{self.project.title} - {self.report_type}"
