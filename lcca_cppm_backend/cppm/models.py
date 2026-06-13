1df57cf02a181cf0107e3ae5e7677f6196922ce8
SHABAN171 <shabanindekupe@gmail.com>
2026-06-13 07:56:09 +0300

    models.py for cppm was added

--- lcca_cppm_backend/cppm/models.py
+++ lcca_cppm_backend/cppm/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from projects.models import Project
 
-# Create your models here.
+
+class PortfolioAnalysis(models.Model):
+
+    project = models.OneToOneField(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='portfolio_analysis'
+    )
+
+    expected_return = models.DecimalField(
+        max_digits=15,
+        decimal_places=2
+    )
+
+    investment_cost = models.DecimalField(
+        max_digits=15,
+        decimal_places=2
+    )
+
+    roi = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+
+    risk_score = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        default=0
+    )
+
+    priority_score = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        default=0
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.project.title
