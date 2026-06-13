d243ffb0273a621a8b884c466d71cc3096d1ffbc
SHABAN171 <shabanindekupe@gmail.com>
2026-06-13 08:19:40 +0300

    Create API View

--- lcca_cppm_backend/cppm/views.py
+++ lcca_cppm_backend/cppm/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import PortfolioAnalysis
+from .serializers import PortfolioAnalysisSerializer
+
+
+class PortfolioAnalysisViewSet(viewsets.ModelViewSet):
+
+    queryset = PortfolioAnalysis.objects.all()
+    serializer_class = PortfolioAnalysisSerializer
+    permission_classes = [IsAuthenticated]
