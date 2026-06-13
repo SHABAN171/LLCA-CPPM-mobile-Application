8120d364232ca1c7c0ff1fb9c5ea34982c3f0dbf
SHABAN171 <shabanindekupe@gmail.com>
2026-06-13 08:22:49 +0300

    views.py updated to Add Ranking Endpoint

--- lcca_cppm_backend/cppm/views.py
+++ lcca_cppm_backend/cppm/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import PortfolioAnalysis
 from .serializers import PortfolioAnalysisSerializer
@@ -11,3 +13,16 @@ class PortfolioAnalysisViewSet(viewsets.ModelViewSet):
     queryset = PortfolioAnalysis.objects.all()
     serializer_class = PortfolioAnalysisSerializer
     permission_classes = [IsAuthenticated]
+@action(detail=False, methods=['get'])
+def rankings(self, request):
+
+    projects = PortfolioAnalysis.objects.order_by(
+        '-priority_score'
+    )
+
+    serializer = self.get_serializer(
+        projects,
+        many=True
+    )
+
+    return Response(serializer.data)
